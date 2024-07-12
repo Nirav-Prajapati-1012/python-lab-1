3dc15865620bb84a9aec578a75168e1e3a60bd69
Nirav-Prajapati-1012 <bca2022nirav1635@tnraocollege.org>
2024-07-12 09:05:56 +0530

    Add files via upload

---
+++ pro1.py
@@ -0,0 +1,10 @@
+a = int(input("Enter number one : "))
+b = int(input("Enter number two : "))
+add = a+b
+print("Addition : ",add)
+sub = a-b
+print("Subtraction : ",sub)
+mul = a*b
+print("Addition : ",mul)
+div = a/b
+print("Addition : ",div)
